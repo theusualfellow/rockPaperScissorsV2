66159ac6f835c9c448206a19360ebec79b15cd20
sandeep <sandeepsingh23ok@gmail.com>
2025-08-19 00:07:32 -0300

    Add basic functionality

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+let humanScore=0, computerScore = 0;
+
 function getComputerChoice(){
     let options = ['rock', 'paper', 'scissors'];
-    console.log(options[Math.floor(Math.random()*3)])
+    return(options[Math.floor(Math.random()*3)])
 }
 
 function getHumanChoice(){
     let humanChoice = prompt("enter your choice");
     return humanChoice;
 }
-console.log(getHumanChoice());
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice===computerChoice){
+        console.log("its a tie")
+    }
+    else if( (humanChoice=="rock" && computerChoice == "scissors")||
+        (humanChoice=="paper" && computerChoice=="rock")||
+        (humanChoice=="scissors"&& computerChoice=="paper")
+
+    ){
+        console.log("human won")
+    }
+    else{
+        console.log(computerChoice);
+
+        console.log(humanChoice)
+        console.log("you lost");
+    }
+}
+
+
+
+playRound(getHumanChoice(),getComputerChoice());
