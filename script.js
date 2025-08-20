4e38e9477fabc18eff7c728264b98aa7bacaa03a
sandeep <sandeepsingh23ok@gmail.com>
2025-08-20 19:39:41 -0300

    Add function to play 5 rounds

--- script.js
+++ script.js
@@ -14,22 +14,42 @@ function getHumanChoice(){
 function playRound(humanChoice, computerChoice){
     if(humanChoice===computerChoice){
         console.log("its a tie")
+        console.log("player: " + humanScore)
+        console.log("computer: " + computerScore) 
     }
     else if( (humanChoice=="rock" && computerChoice == "scissors")||
         (humanChoice=="paper" && computerChoice=="rock")||
         (humanChoice=="scissors"&& computerChoice=="paper")
 
     ){
-        console.log("human won")
-    }
+        console.log("you won")
+        humanScore++;
+        console.log("player: " + humanScore)
+        console.log("computer: " + computerScore)    }
     else{
-        console.log(computerChoice);
 
-        console.log(humanChoice)
         console.log("you lost");
+        computerScore++;
+        console.log("player: " + humanScore)
+        console.log("computer: " + computerScore)
     }
 }
 
-
-
+function playGame(){
 playRound(getHumanChoice(),getComputerChoice());
+playRound(getHumanChoice(),getComputerChoice());
+playRound(getHumanChoice(),getComputerChoice());
+playRound(getHumanChoice(),getComputerChoice());
+playRound(getHumanChoice(),getComputerChoice());
+if(humanScore==computerScore){
+    console.log("the game is tied");
+}
+else if(humanScore>computerScore){
+    console.log("human won the game");
+}
+else{
+    console.log("computer won the game");
+}
+
+}
+playGame();
